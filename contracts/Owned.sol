f654ac4c345a82b7e8d88215430525d76492e5ea
keorn <pczaban@gmail.com>
2017-10-19 18:40:35 +0100

    make coreveto compile, clean badgereg

--- contracts/Owned.sol
+++ contracts/Owned.sol
@@ -2,14 +2,14 @@
 //! By Gav Wood (Ethcore), 2016.
 //! Released under the Apache Licence 2.
 
-pragma solidity ^0.4.6;
+pragma solidity ^0.4.17;
 
 contract Owned {
     modifier only_owner { require (msg.sender == owner); _; }
 
     event NewOwner(address indexed old, address indexed current);
 
-    function setOwner(address _new) only_owner { NewOwner(owner, _new); owner = _new; }
+    function setOwner(address _new) only_owner public { NewOwner(owner, _new); owner = _new; }
 
     address public owner = msg.sender;
 }
